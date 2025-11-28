abb1f6e3287c77fc705e68132d944011c448960c
LuisTrujilloP <luisjosetrujillodeveloper@gmail.com>
2025-11-27 19:17:48 -0500

    initial firebase setup

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    reactStrictMode: true,
-    optimizeFonts: true,
-    images: {
-        deviceSizes: [360, 480, 576, 768, 1200, 1920, 2048, 3840],
-        domains: ['res.cloudinary.com'],
-        loader: 'akamai',
-        path: '/',
-    },
+	reactStrictMode: true,
+	swcMinify: true,
+	output: "export",
+	images: {
+		unoptimized: true, // ← ADD THIS for static export
+		deviceSizes: [360, 480, 576, 768, 1200, 1920, 2048, 3840],
+		domains: ["res.cloudinary.com"],
+	},
 };
 
 module.exports = nextConfig;
